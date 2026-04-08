e25ba98af056d2d412804f18cf5a3072ad078b88
fartlord123 <condezhannahceline@gmail.com>
2026-04-08 18:03:02 +0800

    Update website rebuild

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"247ce47aabb58cceb4c9e5203757185db8daf2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "86961555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2181799132" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
