494ceec035c5e626829860c6a0cde1849e604205
fartlord123 <condezhannahceline@gmail.com>
2026-04-09 15:12:15 +0800

    first deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"247ce47aabb58cceb4c9e5203757185db8daf2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2416973567" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1632719184" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
